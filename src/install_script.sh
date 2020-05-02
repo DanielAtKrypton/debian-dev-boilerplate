994bec7ef89d356e2411ea66f137df382477220f
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2020-05-01 21:01:53 -0300

    :bug: Switch from curl to wget.

--- src/install_script.sh
+++ src/install_script.sh
@@ -5,7 +5,7 @@
 sudo apt install zsh
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # When asked:
 # Time to change your default shell to zsh:
 # Do you want to change your default shell to zsh? [Y/n] y
@@ -17,7 +17,7 @@ sudo apt-get install zsh-antigen
 # or use git.io/antigen-nightly for the latest version
 
 # Configure Antigen
-curl https://github.com/DanielAtKrypton/debian-dev-boilerplate/raw/master/src/.antigenrc -o ~/.antigenrc
+wget https://github.com/DanielAtKrypton/debian-dev-boilerplate/raw/master/src/.antigenrc -o ~/.antigenrc
 
 # Config zsh to use Antigen
-curl https://github.com/DanielAtKrypton/debian-dev-boilerplate/raw/master/src/.zshrc -o ~/.zshrc
+wget https://github.com/DanielAtKrypton/debian-dev-boilerplate/raw/master/src/.zshrc -o ~/.zshrc
