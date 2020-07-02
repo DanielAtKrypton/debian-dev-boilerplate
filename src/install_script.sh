6eff32749e332da25373dfd609aec38712a9de7c
Daniel Kaminski de Souza <51767368+DanielAtKrypton@users.noreply.github.com>
2020-07-02 01:46:41 -0300

    Update install_script.sh

--- src/install_script.sh
+++ src/install_script.sh
@@ -4,19 +4,10 @@
 # wsl --set-version Debian 2
 
 . /etc/os-release
-distribution=$(. /etc/os-release; echo $ID$VERSION_ID)
 distribution_id=$(. /etc/os-release; echo $ID)
 
 # https://ubuntu.com/blog/getting-started-with-cuda-on-ubuntu-on-wsl-2
-yes | sudo apt-get install curl gnupg
-
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container-experimental.list | sudo tee /etc/apt/sources.list.d/libnvidia-container-experimental.list
-
-sudo apt update && sudo apt install -y nvidia-docker2
+yes | sudo apt-get install curl
 
 # install zsh
 sudo apt install zsh
