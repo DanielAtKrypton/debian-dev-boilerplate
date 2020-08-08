5c49cdc9697a6e8f3a3de8e861f6ef8a1b9e9171
Daniel Kaminski de Souza <51767368+DanielAtKrypton@users.noreply.github.com>
2020-08-08 01:19:14 +0100

    Update install_script.sh

--- src/install_script.sh
+++ src/install_script.sh
@@ -7,17 +7,17 @@
 distribution_id=$(. /etc/os-release; echo $ID)
 
 # https://ubuntu.com/blog/getting-started-with-cuda-on-ubuntu-on-wsl-2
-yes | sudo apt-get install curl netcat
+yes | apt-get install curl netcat
 
 # install zsh
-sudo apt install zsh
+apt install zsh
 
 # install oh-my-zsh
 yes | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install Antigen
 if [ $distribution_id = "debian" ]; then
-  sudo apt-get install zsh-antigen
+  apt-get install zsh-antigen
 elif [ $distribution_id = "ubuntu" ]; then
   curl -L git.io/antigen > $HOME/antigen.zsh
 else
@@ -36,6 +36,6 @@ rm ~/.zshrc_extra
 
 
 # Now set ZSH as the default login shell for the user you’re logged in as with the following command:
-sudo usermod -s /usr/bin/zsh $(whoami)
+usermod -s /usr/bin/zsh $(whoami)
 
 exit
