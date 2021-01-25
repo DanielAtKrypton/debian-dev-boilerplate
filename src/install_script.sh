900f3ea270920060e47f9cd7aae0de1d4d172dce
Daniel Kaminski de Souza <51767368+DanielAtKrypton@users.noreply.github.com>
2021-01-25 18:12:41 +0000

    Update install_script.sh

--- src/install_script.sh
+++ src/install_script.sh
@@ -3,17 +3,26 @@
 # Update to wsl 2. From powershell, run the following:
 # wsl --set-version Debian 2
 
+apt update
+apt upgrade
+
 . /etc/os-release
 distribution_id=$(. /etc/os-release; echo $ID)
 
 # https://ubuntu.com/blog/getting-started-with-cuda-on-ubuntu-on-wsl-2
-yes | apt-get install curl netcat
+apt-get install curl netcat
 
 # install zsh
 apt install zsh
 
+# install wget
+apt install wget
+
+# install git
+apt install git
+
 # install oh-my-zsh
-yes | sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install Antigen
 if [ $distribution_id = "debian" ]; then
@@ -36,9 +45,9 @@ rm ~/.zshrc_extra
 
 
 # Now set ZSH as the default login shell for the user you’re logged in as with the following command:
-sudo chmod gu+w ~/.zshrc
-sudo chown $(whoami) ~/.zshrc
+# sudo chmod gu+w ~/.zshrc
+# sudo chown $(whoami) ~/.zshrc
 
-sudo usermod -s /usr/bin/zsh $(whoami)
+# sudo usermod -s /usr/bin/zsh $(whoami)
 
 exit
